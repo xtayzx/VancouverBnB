d74a7756848d4d06fe42b352ce6f5141c7e45833
nylcheung <nylcheung2000@gmail.com>
2023-12-12 20:57:58 -0800

    host about secction completed

--- host.php
+++ host.php
@@ -37,9 +37,11 @@ if ($search_result->fetch_row() != 0) {
 
         //IMAGE
         echo "<div class =\"host-info\">";
+        echo "<div class =\"host-info-card\">";
         echo "<div class =\"host-details\">";
         echo "<div class =\"host-img-container\">";
         echo "<img class=\"host-img\" src=\"" . $row["host_picture_url"] . "\">";
+        echo "</div>";
         //NAME
         echo "<h2>" . $row["host_name"] . "</h2>";
         echo "<br>";
@@ -47,53 +49,65 @@ if ($search_result->fetch_row() != 0) {
         echo "<p><a href=\"" . $row["host_url"] . "\">Airbnb Profile Link</a></p>";
         echo "<br>";
 
-        // echo "<div class=\"host-stats\">";
+
+
         //HOST RATES
-        
-        echo "<p class = \"stats\">" . $row["host_response_rate"] . "</p><br>";
-        echo "<h3>Response Rate</h3>";
-        echo "<br>";
-        echo "<p class = \"stats\">" . $row["host_acceptance_rate"] . "</p>";
-        echo "<h3>Acceptance Rate</h3>";
-        echo "<br>";
+        echo "<div class=\"host-stats\">";
+        echo "<div class=\"rate\">";
+        echo "<p class=\"stats\"><strong>" . $row["host_response_rate"] . "</strong></p>";
+        echo "<p>Response Rate</p>";
+        echo "</div>";
 
-        //HOST LISITINGS
-        echo "<h3>Listings Count</h3>";
-        echo "<p class = \"stats\">" . $row["host_listings_count"] . "</p><br>";
-        echo "<h3>Total Listings Count</h3>";
-        echo "<p class = \"stats\">" . $row["host_total_listings_count"] . "</p>";
-        echo "<br>";
+        // echo "<p class =\"host-stats-divider\">|</p>";
+        echo "<div class=\"rate\">";
+        echo "<p class=\"stats\"><strong>" . $row["host_acceptance_rate"] . "</strong></p>";
+        echo "<p>Acceptance Rate</p>";
+        echo "</div>";
+        echo "</div>";
 
+        //HOST LISTINGS
+        echo "<div class=\"host-stats\">";
+        echo "<div class=\"rate\">";
+        echo "<span class=\"stats\"><strong>" . $row["host_listings_count"] . "</strong></p>";
+        echo "<p>Listings Count</p>";
+        echo "</div>";
+
+        echo "<div class=\"rate\">";
+        echo "<span class=\"stats\"><strong>" . $row["host_total_listings_count"] . "</strong></p>";
+        echo "<p>Total Listings Count</p>";
+        echo "</div>";
+        echo "</div>";
 
         echo "</div>";
         echo "</div>";
-        // echo "</div>";
 
 
+        // Host ID, Host Since, Host Location
+        echo "<div class=\"about-section\">";
+        echo "<h3 class =\"host-about-name\">About ". $row["host_name"] . "</h3>";
 
-        //HOST ID
-        echo "<h3>Host ID</h3>";
-        echo "<p><a href=\"host.php?id=" . $row["host_id"] . "\">" . $row["host_id"] . "</a></p>";
-        echo "<br>";
 
-        //HOST SINCE
-        echo "<h3>Host Since</h3>";
-        echo "<p>" . $row["host_since"] . "</p>";
-        echo "<br>";
-
-        //ABOUT
-        echo "<h3>About</h3>";
-        echo "<p>" . $row["host_about"] . "</p>";
-        echo "<br>";
-
-        //HOST LOCATION
+        echo "<div class=\"host-details-info\">";
+        echo "<div class=\"column\">";
         echo "<h3>Host Location</h3>";
         echo "<p>" . $row["host_location"] . "</p>";
-        echo "<br>";
-
-        // echo "<div class =\"host-rates\">";
-
-
+        echo "</div>";
+        
+        echo "<div class=\"column\">";
+        echo "<h3>Host Since</h3>";
+        echo "<p>" . $row["host_since"] . "</p>";
+        echo "</div>";
+        
+        echo "<div class=\"column\">";
+        echo "<h3>Host ID</h3>";
+        echo "<p><a href=\"host.php?id=" . $row["host_id"] . "\">" . $row["host_id"] . "</a></p>";
+        echo "</div>";
+        echo "</div>";
+        
+        echo "<p class =\"about\">" . $row["host_about"] . "</p>";
+        echo "</div>";
+    
+        
     }
 }
 
