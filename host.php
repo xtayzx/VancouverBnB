b2d7e134ac43899f6553aca9dd7aff6009dcf31d
nylcheung <nylcheung2000@gmail.com>
2023-12-12 17:53:06 -0800

    currently working on the design of the host page

--- host.php
+++ host.php
@@ -1,101 +1,112 @@
 <?php
-    require_once("private/initialize.php");
+require_once("private/initialize.php");
 
-    //Identify what is passed in the URL
-    $code = trim($_GET["id"]);
-    @$msg = trim($_GET["message"]);
+//Identify what is passed in the URL
+$code = trim($_GET["id"]);
+@$msg = trim($_GET["message"]);
 
-    $page_title = "Host Details";
-    require("header.php");
- ?>
-
-<div class="page-content">
+$page_title = "Host Details";
+require("header.php");
+?>
 
 
-    <?php
-    $host_query = "SELECT * FROM hosts WHERE hosts.host_id = ?";
-    $stmt = $db->prepare($host_query);
 
-    //check for query error
-    if(!$stmt) {
-        die("Error is:".$db->error);
-    }
+<?php
+$host_query = "SELECT * FROM hosts WHERE hosts.host_id = ?";
+$stmt = $db->prepare($host_query);
 
-    $stmt->bind_param('s',$code);
-    $stmt->execute();
-    $search_result = $stmt->get_result();
+//check for query error
+if (!$stmt) {
+    die("Error is:" . $db->error);
+}
 
-    if (!empty($msg) ) {
-        echo "<p>$msg</p><br>\n";
-    }
+$stmt->bind_param('s', $code);
+$stmt->execute();
+$search_result = $stmt->get_result();
 
-    //start the table of details
-    if($search_result->fetch_row() != 0) {
+if (!empty($msg)) {
+    echo "<p>$msg</p><br>\n";
+}
 
-        //has to go back to the first of the array
-        $search_result->data_seek(0);
-        while($row = $search_result->fetch_assoc()) {
+//start the table of details
+if ($search_result->fetch_row() != 0) {
+
+    //has to go back to the first of the array
+    $search_result->data_seek(0);
+    while ($row = $search_result->fetch_assoc()) {
 
         //IMAGE
-        echo "<img class=\"host-img\" src=\"".$row["host_picture_url"]."\">";
-
+        echo "<div class =\"host-info\">";
+        echo "<div class =\"host-details\">";
+        echo "<div class =\"host-img-container\">";
+        echo "<img class=\"host-img\" src=\"" . $row["host_picture_url"] . "\">";
         //NAME
-        echo "<h2>".$row["host_name"]."</h2>";
+        echo "<h2>" . $row["host_name"] . "</h2>";
         echo "<br>";
-
         //HOST URL
-        echo "<h3>Airbnb URL</h3>";
-        echo "<p><a href=\"".$row["host_url"]."\">Click Here</a></p>";
+        echo "<p><a href=\"" . $row["host_url"] . "\">Airbnb Profile Link</a></p>";
         echo "<br>";
 
-        //HOST ID
-        echo "<h3>Host ID</h3>";
-        echo "<p><a href=\"host.php?id=".$row["host_id"]."\">".$row["host_id"]."</a></p>";
-        echo "<br>";
-
-        //HOST SINCE
-        echo "<h3>Host Since</h3>";
-        echo "<p>".$row["host_since"]."</p>";
-        echo "<br>";
-
-        //ABOUT
-        echo "<h3>About</h3>";
-        echo "<p>".$row["host_about"]."</p>";
-        echo "<br>";
-
-        //HOST LOCATION
-        echo "<h3>Host Location</h3>";
-        echo "<p>".$row["host_location"]."</p>";
-        echo "<br>";
-
+        // echo "<div class=\"host-stats\">";
         //HOST RATES
-        echo "<h3>Repsonse Rate</h3>";
-        echo "<p>".$row["host_response_rate"]."</p><br>";
+        
+        echo "<p class = \"stats\">" . $row["host_response_rate"] . "</p><br>";
+        echo "<h3>Response Rate</h3>";
+        echo "<br>";
+        echo "<p class = \"stats\">" . $row["host_acceptance_rate"] . "</p>";
         echo "<h3>Acceptance Rate</h3>";
-        echo "<p>".$row["host_acceptance_rate"]."</p>";
         echo "<br>";
 
         //HOST LISITINGS
         echo "<h3>Listings Count</h3>";
-        echo "<p>".$row["host_listings_count"]."</p><br>";
+        echo "<p class = \"stats\">" . $row["host_listings_count"] . "</p><br>";
         echo "<h3>Total Listings Count</h3>";
-        echo "<p>".$row["host_total_listings_count"]."</p>";
+        echo "<p class = \"stats\">" . $row["host_total_listings_count"] . "</p>";
         echo "<br>";
 
-       
-        }
-    }
-        
-    //if the table cannot be generated
-    else  {
-        echo "<p>The entry cannot be found.</p>";
-    }
-
-    $stmt->free_result();
-
-        $db->close();
 
         echo "</div>";
+        echo "</div>";
+        // echo "</div>";
 
-        include("footer.php");
-    ?>
+
+
+        //HOST ID
+        echo "<h3>Host ID</h3>";
+        echo "<p><a href=\"host.php?id=" . $row["host_id"] . "\">" . $row["host_id"] . "</a></p>";
+        echo "<br>";
+
+        //HOST SINCE
+        echo "<h3>Host Since</h3>";
+        echo "<p>" . $row["host_since"] . "</p>";
+        echo "<br>";
+
+        //ABOUT
+        echo "<h3>About</h3>";
+        echo "<p>" . $row["host_about"] . "</p>";
+        echo "<br>";
+
+        //HOST LOCATION
+        echo "<h3>Host Location</h3>";
+        echo "<p>" . $row["host_location"] . "</p>";
+        echo "<br>";
+
+        // echo "<div class =\"host-rates\">";
+
+
+    }
+}
+
+//if the table cannot be generated
+else {
+    echo "<p>The entry cannot be found.</p>";
+}
+
+$stmt->free_result();
+
+$db->close();
+
+echo "</div>";
+
+include("footer.php");
+?>
--- listingdetails.php
+++ listingdetails.php
@@ -1,40 +1,40 @@
 <?php
-    require_once("private/initialize.php");
-    date_default_timezone_set('UTC');
+require_once("private/initialize.php");
+date_default_timezone_set('UTC');
 
-    //Identify what is passed in the URL
-    $code = trim($_GET["id"]);
-    @$msg = trim($_GET["message"]);
+//Identify what is passed in the URL
+$code = trim($_GET["id"]);
+@$msg = trim($_GET["message"]);
 
-    $host_id = '';
+$host_id = '';
 
-    $page_title = "Listing Details";
-    require("header.php");
+$page_title = "Listing Details";
+require("header.php");
 
-    if(is_post_request()) {
-        $t = date("Y-m-d H:i:s");
-        $sql = "INSERT INTO comments (listing_id, timestamp, username, comment) 
+if (is_post_request()) {
+    $t = date("Y-m-d H:i:s");
+    $sql = "INSERT INTO comments (listing_id, timestamp, username, comment) 
         VALUES (?,?,?,?)";
-        $stmt = mysqli_prepare($db, $sql);
-        mysqli_stmt_bind_param($stmt,"ssss", 
+    $stmt = mysqli_prepare($db, $sql);
+    mysqli_stmt_bind_param($stmt, "ssss",
         $code,
         $t,
         $_SESSION["valid_user"],
         $_POST["comment"]);
 
-        $stmt = $db->prepare($sql);
-        $stmt->bind_param('ssss', $code, $t, $_SESSION["valid_user"], $_POST["comment"]);
-        $res = $stmt->execute();
+    $stmt = $db->prepare($sql);
+    $stmt->bind_param('ssss', $code, $t, $_SESSION["valid_user"], $_POST["comment"]);
+    $res = $stmt->execute();
 
-        // if($res){
-        //     // $_SESSION["valid_user"] = $_POST["username"];
-        //     // $_SESSION["neighbourhood_preference"] = $_POST["neighbourhood_preference"];
-        //     // header("Location: listings.php");
-        // }
+    // if($res){
+    //     // $_SESSION["valid_user"] = $_POST["username"];
+    //     // $_SESSION["neighbourhood_preference"] = $_POST["neighbourhood_preference"];
+    //     // header("Location: listings.php");
+    // }
 
-        $stmt->free_result();
-    }
- ?>
+    $stmt->free_result();
+}
+?>
 
 <div class="page-content">
     <?php
@@ -42,111 +42,120 @@
     $stmt = $db->prepare($listing_display_query);
 
     //check for query error
-    if(!$stmt) {
-        die("Error is:".$db->error);
+    if (!$stmt) {
+        die("Error is:" . $db->error);
     }
 
-    $stmt->bind_param('s',$code);
+    $stmt->bind_param('s', $code);
     $stmt->execute();
     $search_result = $stmt->get_result();
 
-    if (!empty($msg) ) {
+    if (!empty($msg)) {
         echo "<p>$msg</p><br>\n";
     }
 
     //start the table of details
-    if($search_result->fetch_row() != 0) {
+    if ($search_result->fetch_row() != 0) {
 
         //has to go back to the first of the array
         $search_result->data_seek(0);
-        while($row = $search_result->fetch_assoc()) {
+        while ($row = $search_result->fetch_assoc()) {
 
-        //IMAGE
-        echo "<img class=\"image-container\" src=\"".$row["picture_url"]."\">";
+            echo "<div class = \"listing-info\">";
+            echo "<div class = \"image-column\">";
 
-        //NAME
-        echo "<h2>".$row["name"]."</h2>";
-        echo "<br>";
+            //IMAGE
+            echo "<img class=\"image-container\" src=\"" . $row["picture_url"] . "\">";
+            echo "<h2>" . $row["name"] . "</h2>";
+            echo "<br>";
+            echo "</div>";
 
-        //HOST ID
-        // echo "<h3>Host ID</h3>";
-        // echo "<p><a href=\"host.php?id=".$row["host_id"]."\">".$row["host_id"]."</a></p>";
-        $host_id = $row["host_id"];
-        // echo "<br>";
+            echo "<div class = \"info-column\">";
+            //NAME
+            
 
-        //DESCRIPTION
-        echo "<h3>Description</h3>";
-        echo "<p>".$row["description"]."</p>";
-        echo "<br>";
+            //HOST ID
+            // echo "<h3>Host ID</h3>";
+            // echo "<p><a href=\"host.php?id=".$row["host_id"]."\">".$row["host_id"]."</a></p>";
+            $host_id = $row["host_id"];
+            // echo "<br>";
+    
+            //DESCRIPTION
+            echo "<h3>Description</h3>";
+            echo "<p>" . $row["description"] . "</p>";
+            echo "<br>";
 
-        //NEIGHBOURHOOD DESCRIPTION
-        echo "<h3>Neighbourhood Description</h3>";
-        echo "<p>".$row["neighborhood_description"]."</p>";
-        echo "<br>";
+            //NEIGHBOURHOOD DESCRIPTION
+            echo "<h3>Neighbourhood Description</h3>";
+            echo "<p>" . $row["neighborhood_description"] . "</p>";
+            echo "<br>";
 
-        //LOCATION COORDINATES
-        echo "<h3>Lat and Long</h3>";
-        echo "<p>Lat: ".$row["latitude"]."</p>";
-        echo "<p>Long: ".$row["longitude"]."</p>";
-        echo "<br>";
+            //LOCATION COORDINATES
+            echo "<h3>Lat and Long</h3>";
+            echo "<p>Lat: " . $row["latitude"] . "</p>";
+            echo "<p>Long: " . $row["longitude"] . "</p>";
+            echo "<br>";
 
-        //PROPERTY TYPE
-        echo "<h3>Property Type</h3>";
-        echo "<p>".$row["property_type"]."</p>";
-        echo "<br>";
+            //PROPERTY TYPE
+            echo "<h3>Property Type</h3>";
+            echo "<p>" . $row["property_type"] . "</p>";
+            echo "<br>";
 
-        //ROOM TYPE
-        echo "<h3>Property Type</h3>";
-        echo "<p>".$row["room_type"]."</p>";
-        echo "<br>";
+            //ROOM TYPE
+            echo "<h3>Property Type</h3>";
+            echo "<p>" . $row["room_type"] . "</p>";
+            echo "<br>";
 
-        //GUESTS
-        echo "<h3>Accomodates</h3>";
-        echo "<p>".$row["accommodates"]."</p>";
-        echo "<br>";
+            //GUESTS
+            echo "<h3>Accomodates</h3>";
+            echo "<p>" . $row["accommodates"] . "</p>";
+            echo "<br>";
 
-        //PRICE
-        echo "<h3>Price</h3>";
-        echo "<p>".$row["price"]."</p>";
-        echo "<br>";
+            //PRICE
+            echo "<h3>Price</h3>";
+            echo "<p>" . $row["price"] . "</p>";
+            echo "<br>";
 
-        //NIGHTS
-        echo "<h3>Max and Min Nights</h3>";
-        echo "<p>Min Nights: ".$row["minimum_nights"]."</p>";
-        echo "<p>Max Nights: ".$row["maximum_nights"]."</p>";
-        echo "<br>";
+            //NIGHTS
+            echo "<h3>Max and Min Nights</h3>";
+            echo "<p>Min Nights: " . $row["minimum_nights"] . "</p>";
+            echo "<p>Max Nights: " . $row["maximum_nights"] . "</p>";
+            echo "<br>";
 
-        //NUMBER OF REVIEWS
-        echo "<h3>Reviews</h3>";
-        echo "<p>Number of Reviews: ".$row["number_of_reviews"]."</p>";
-        echo "<p>First Review: ".$row["first_review"]."</p>";
-        echo "<p>Last Review: ".$row["last_review"]."</p>";
-        echo "<br>";
+            //NUMBER OF REVIEWS
+            echo "<h3>Reviews</h3>";
+            echo "<p>Number of Reviews: " . $row["number_of_reviews"] . "</p>";
+            echo "<p>First Review: " . $row["first_review"] . "</p>";
+            echo "<p>Last Review: " . $row["last_review"] . "</p>";
+            echo "<br>";
 
-        //NUMBER OF REVIEWS
-        echo "<h3>Review Score Ratings</h3>";
-        echo "<p>Overall Rating: ".$row["review_scores_rating"]."</p>";
-        echo "<p>Accuracy: ".$row["review_scores_accuracy"]."</p>";
-        echo "<p>Cleanliness: ".$row["review_scores_cleanliness"]."</p>";
-        echo "<p>Check-In: ".$row["review_scores_checkin"]."</p>";
-        echo "<p>Communication: ".$row["review_scores_communication"]."</p>";
-        echo "<p>Location: ".$row["review_scores_location"]."</p>";
-        echo "<p>Value: ".$row["review_scores_value"]."</p>";
-        echo "<p>Reviews Per Month: ".$row["reviews_per_month"]."</p>";
-        echo "<br>";
+            //NUMBER OF REVIEWS
+            echo "<h3>Review Score Ratings</h3>";
+            echo "<p>Overall Rating: " . $row["review_scores_rating"] . "</p>";
+            echo "<p>Accuracy: " . $row["review_scores_accuracy"] . "</p>";
+            echo "<p>Cleanliness: " . $row["review_scores_cleanliness"] . "</p>";
+            echo "<p>Check-In: " . $row["review_scores_checkin"] . "</p>";
+            echo "<p>Communication: " . $row["review_scores_communication"] . "</p>";
+            echo "<p>Location: " . $row["review_scores_location"] . "</p>";
+            echo "<p>Value: " . $row["review_scores_value"] . "</p>";
+            echo "<p>Reviews Per Month: " . $row["reviews_per_month"] . "</p>";
+            echo "<br>";
 
-        //ORIGINAL POSTING
-        echo "<h3>Original Posting</h3>";
-        echo "<p><a href=\"".$row["listing_url"]."\">Click Here</a></p>";
-        echo "<br>";
+            //ORIGINAL POSTING
+            echo "<h3>Original Posting</h3>";
+            echo "<p><a href=\"" . $row["listing_url"] . "\">Click Here</a></p>";
+            echo "<br>";
 
-        // echo "<div id =”my-map” style = “width:800px; height:600px;”><p>This Map</p></div>";
+            echo "</div>";
+            echo "</div>";
 
+            // echo "<div id =”my-map” style = “width:800px; height:600px;”><p>This Map</p></div>";
+    
         }
     }
-        
+
     //if the table cannot be generated
-    else  {
+    else {
         echo "<p>The entry cannot be found.</p>";
     }
 
@@ -156,34 +165,34 @@
     $stmt = $db->prepare($host_query);
 
     //check for query error
-    if(!$stmt) {
-        die("Error is:".$db->error);
+    if (!$stmt) {
+        die("Error is:" . $db->error);
     }
 
-    $stmt->bind_param('s',$host_id);
+    $stmt->bind_param('s', $host_id);
     $stmt->execute();
     $search_result = $stmt->get_result();
 
-    if (!empty($msg) ) {
+    if (!empty($msg)) {
         echo "<p>$msg</p><br>\n";
     }
 
     //start the table of details
-    if($search_result->fetch_row() != 0) {
+    if ($search_result->fetch_row() != 0) {
 
         //has to go back to the first of the array
         $search_result->data_seek(0);
-        while($row = $search_result->fetch_assoc()) {
+        while ($row = $search_result->fetch_assoc()) {
 
-        //IMAGE
-        echo "<img class=\"thumb-img\" src=\"".$row["host_thumbnail_url"]."\">";
-        echo "<p>Airbnb Host: <a href=\"host.php?id=".$row["host_id"]."\">".$row["host_name"]."</a></p>";
-        echo "<br>";
+            //IMAGE
+            echo "<img class=\"thumb-img\" src=\"" . $row["host_thumbnail_url"] . "\">";
+            echo "<p>Airbnb Host: <a href=\"host.php?id=" . $row["host_id"] . "\">" . $row["host_name"] . "</a></p>";
+            echo "<br>";
         }
     }
-        
+
     //if the table cannot be generated
-    else  {
+    else {
         echo "<p>The entry cannot be found.</p>";
     }
 
@@ -191,14 +200,14 @@
 
 
     //////
-
+    
     //if the product is not in the watchlist, else if there is a message to display, else if the user is logged in
-    if(!is_in_watchlist($code) ) {
+    if (!is_in_watchlist($code)) {
         echo "<form action=\"addtowatchlist.php\" method=\"post\">\n";
         echo "<input type=\"hidden\" name=\"id\" value=$code>\n";
         echo "<input type=\"submit\" class=\"main-button\" value=\"Add To Watchlist\">\n";
         echo "</form>\n";
-    } 
+    }
     // else if (!empty($msg) ) {
     //     echo "<p>$msg</p>\n";
     // } 
@@ -210,64 +219,62 @@
     ?>
 
 
-        <h3>All Comments</h3><br>
+    <h3>All Comments</h3><br>
 
-        <?php
-            $comment_query = "SELECT * FROM comments WHERE comments.listing_id = ?";
-            $stmt = $db->prepare($comment_query);
+    <?php
+    $comment_query = "SELECT * FROM comments WHERE comments.listing_id = ?";
+    $stmt = $db->prepare($comment_query);
 
-            //check for query error
-            if(!$stmt) {
-                die("Error is:".$db->error);
-            }
+    //check for query error
+    if (!$stmt) {
+        die("Error is:" . $db->error);
+    }
 
-            $stmt->bind_param('s',$code);
-            $stmt->execute();
-            $search_result = $stmt->get_result();
+    $stmt->bind_param('s', $code);
+    $stmt->execute();
+    $search_result = $stmt->get_result();
 
-            if (!empty($msg) ) {
-                echo "<p>$msg</p>\n";
-            }
+    if (!empty($msg)) {
+        echo "<p>$msg</p>\n";
+    }
 
-            //start the table of details
-            if($search_result->fetch_row() != 0) {
+    //start the table of details
+    if ($search_result->fetch_row() != 0) {
 
-                //has to go back to the first of the array
-                $search_result->data_seek(0);
+        //has to go back to the first of the array
+        $search_result->data_seek(0);
 
-                while($row = $search_result->fetch_assoc()) {
+        while ($row = $search_result->fetch_assoc()) {
 
-                //NAME
-                echo "<h5>".$row["username"]." | " .$row["timestamp"]."</h5>";
-                echo "<p>".$row["comment"]."</p>";
-                echo "<br>";
+            //NAME
+            echo "<h5>" . $row["username"] . " | " . $row["timestamp"] . "</h5>";
+            echo "<p>" . $row["comment"] . "</p>";
+            echo "<br>";
 
-                // echo "<div id =”my-map” style = “width:800px; height:600px;”><p>This Map</p></div>";
+            // echo "<div id =”my-map” style = “width:800px; height:600px;”><p>This Map</p></div>";
+    
+        }
+    }
 
-                }
-            }
-                
-            //if the table cannot be generated
-            else  {
-                echo "<p>There are no comments for this listing.</p><br>";
-            }
+    //if the table cannot be generated
+    else {
+        echo "<p>There are no comments for this listing.</p><br>";
+    }
 
-            $stmt->free_result();
+    $stmt->free_result();
 
-            if(is_logged_in()) {
-                echo "<form method=\"post\">";
-                echo "<h4>Submit a Comment</h4><br>";
-                echo "<label>Enter Comment: </label><br>";
-                echo "<textarea name=\"comment\" rows=\"5\" cols=\"60\"></textarea><br><br>";
-                echo "<input type=\"submit\" id=\"submit\" value=\"Submit Comment\"/>";
-                echo "</form></div><br>";
-            }
+    if (is_logged_in()) {
+        echo "<form method=\"post\">";
+        echo "<h4>Submit a Comment</h4><br>";
+        echo "<label>Enter Comment: </label><br>";
+        echo "<textarea name=\"comment\" rows=\"5\" cols=\"60\"></textarea><br><br>";
+        echo "<input type=\"submit\" id=\"submit\" value=\"Submit Comment\"/>";
+        echo "</form></div><br>";
+    } else {
+        echo "</div><br>";
+    }
 
-            else {
-                echo "</div><br>";
-            }
+    $db->close();
 
-        $db->close();
-
-        include("footer.php");
+    include("footer.php");
     ?>
